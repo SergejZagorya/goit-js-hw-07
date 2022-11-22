b8f8c02a07977e3d624b033cb6a21af1de00175d
SergejZagorya <107615722+SergejZagorya@users.noreply.github.com>
2022-11-22 09:50:00 +0200

    Finally

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryItemsMarkup = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(`<img src=${e.target.dataset.source} />`, {
+    onShow: () => {
+      document.addEventListener("keydown", onEscape);
+    },
+
+    onClose: () => {
+      document.removeEventListener("keydown", onEscape);
+    },
+  });
+  instance.show();
+
+  function onEscape(e) {
+    if (e.key === "Escape") instance.close();
+  }
+}
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryItemsMarkup = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</div>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+jjjjjjlll;
